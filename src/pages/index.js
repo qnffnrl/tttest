5c90cc9071aecc14f36140014413d6ad17a3692a
risker <roakfwk30@naver.com>
2024-12-14 18:26:03 +0900

    init

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export * from "@/pages/about.jsx";
+export * from "@/pages/profile";
+export * from "@/pages/sign-in";
+export * from "@/pages/sign-up";
---
+++ src/routes.jsx
@@ -0,0 +1,32 @@
+import { About, Profile, SignIn, SignUp } from "@/pages";
+
+export const routes = [
+  {
+    name: "about",
+    path: "/about",
+    element: <About />,
+  },
+  {
+    name: "profile",
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    name: "Sign In",
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    name: "Sign Up",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  // {
+  //   name: "Docs",
+  //   href: "https://www.material-tailwind.com/docs/react/installation",
+  //   target: "_blank",
+  //   element: "",
+  // },
+];
+
+export default routes;
